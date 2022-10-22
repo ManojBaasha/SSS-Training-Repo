dda6ad42920642c82124f4c1513ed76a184efb2a
username <71690828+UchihaCryptic@users.noreply.github.com>
2022-10-21 19:34:12 -0700

    Pi camera training module

---
+++ Training_Sessions/pi_camera_starter.py
@@ -0,0 +1,20 @@
+import os
+from picamera import PiCamera
+from datetime import datetime
+from time import sleep
+
+camera = PiCamera()
+
+# Objectives:
+# 1. Set the resolution of the picture to QHD (2560x14400)
+# 2. Give the sensor time to focus and adjust before taking a picture
+# 3. Save the image with the date and time as the filename
+# 4. Save the files within an "images" directory (assume the directory exists on the same level as the python script)
+# Bonus: Modify the program to take in user input for the resolution (Adjust the capture function parameters as needed)
+
+def capture():
+    # Implement code here
+    pass
+    
+
+capture()
